80cf8af892b978fe1da24a757d2c02d906fe613c
Sander Ubachs <sanderubachs28@gmail.com>
2021-04-05 13:43:23 +0200

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,68 @@
 <template>
+  <main id="app">
+     <section class="products">
+      <Products 
+        v-for="product in products"
+        :key="product.color"
+        :product="product"
+        /> 
+     </section>
+  </main>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+// import Product from './components/Products.vue';
+import Products from './components/Products';
 
 export default {
-  name: "App",
+  name: "app",
   components: {
     HelloWorld,
+    Products
   },
+    data () {
+    return {
+      products: [
+        {
+          title: '1. No Proverty',
+          color: 'green',
+          bgtext: 'SDG1',
+          src: require('./assets/SDG-1.png'),
+          products: '2h ago',
+          // product: 'heavy rain!'
+          // person who posted pic:
+    
+        },
+         {
+          title: '2. Zero Hunger',
+          color: 'blue',
+          bgtext: 'SDG2',
+          src: require('./assets/SDG-2.png'),
+          products: '3h ago',
+          // product: 'heavy rain!'
+
+        }, 
+        {
+          title: '3. Good Health and Well-being',
+          color: 'red',
+          bgtext: 'SDG3',
+          src: require('./assets/SDG-3.png'),
+          products: '4h ago',
+          // product: 'heavy rain!'
+
+        },
+        ]
+      }
+    },
 };
+
+
+
+
+
 </script>
 
 <style>
@@ -23,4 +74,33 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  text-align: left;
+  }
+
+  body{
+    font-family: 'montserratt', sans-serif;
+    }
+
+   main{
+    width: 100vw;
+    min-height: 100vh;
+    overflow: hidden;
+
+    background-color: #EEE;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+     }
+
+     .products{
+       display: flex;
+       max-width: 1280px;
+       padding: 25px;
+       margin: 0 auto;
+       }
 </style>
---
+++ src/components/Products.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="product">
+        <!-- {{ product.title }} --> 
+        <div :class="'product-inner ' + product.color">
+             <div class="product-text-wrap">
+                 <h2 class="bg-text">{{ product.bgtext }}</h2>
+             </div>
+             <div class="product-img-wrap">
+                 <img :src="product.src" class="image" />
+             </div>
+
+                <div class="product-detail">
+                    <h2>{{product.title}}</h2>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum vitae minus adipisci dicta laudantium corrupti eligendi iure, nesciunt, maxime, culpa praesentium! Nisi, obcaecati tempore. Fuga in amet repudiandae cumque nesciunt!
+                    </p>
+
+                    <div class="time-added">
+                        <p>{{product.products}}</p>  
+                    </div>
+                </div>
+        </div>
+    </div>
+</template>
+
+<script> 
+export default {
+    name: 'product',
+    props:['product']
+    }    
+</script>
+
+<style>
+    .product {
+        flex: 1 1 33.333%;
+        width: 100%;
+        padding: 25px;
+        }
+
+    .product-inner{
+          position: relative;
+          padding: 25px;
+          box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+        }
+
+            .product-image-wrap{
+                width: 100%;
+                }
+
+    .product-inner.green{
+        background-image: linear-gradient(to bottom right, #24d484, #116432);
+        }
+     .product-inner.red{
+        background-image: linear-gradient(to bottom left, #24d484, #2474c4 70%);
+        }
+    .product-inner.blue{
+        background-image: linear-gradient(to bottom right, #f44484, #1168d4);
+        }
+
+    .product-text-wrap{
+        position: absolute;
+        top:0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: 0;
+        overflow: hidden;
+        }    
+
+    .product-text-wrap h2{
+        color: #313131;
+        font-size: 128px;
+        font-weight: 900; 
+        opacity: 0.2;
+        transform-origin: center;
+        }    
+    .product-image-wrap {
+        position: relative;
+        z-index: 1;
+        transform-origin: center;
+        }
+    .product-image-wrap .image{
+        width: 100%;
+        filter: drop-shadow(0px 0px 12px rgba(0, 0, 0, 0.25));
+        }
+
+     .product-detail{
+         background-color: #ffffff;
+         padding: 25px;
+         margin: 0px -25px -25px;
+         }
+
+
+         .product-detail h2 {
+             font-size: 28px;
+             font-weight: 700;
+             color: #454545;
+             margin-bottom: 15px;
+             }
+
+    .product-detail p{
+        font-size: 18px;
+         line-height: 1.5;
+         font-weight: 300;
+         color: #676767;
+         }
+</style>
