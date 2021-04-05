7962f69b20f46d94894beef3c181b0b2a7d10237
Sander Ubachs <sanderubachs28@gmail.com>
2021-04-05 15:50:44 +0200

    Post actions added

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,13 @@
         :product="product"
         /> 
      </section>
+    <section class="cards">
+      <PostCard 
+        v-for="card in cards"
+        :key="card.color"
+        :card="card"
+        /> 
+     </section>
   </main>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
@@ -14,43 +21,80 @@
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+// import PostCard from './components/PostCard.vue';
 // import Product from './components/Products.vue';
 import Products from './components/Products';
+import PostCard from './components/PostCard';
 
 export default {
   name: "app",
   components: {
     HelloWorld,
-    Products
+    Products,
+    PostCard,
   },
     data () {
     return {
-      products: [
+      // products: [
+      //   {
+      //     title: '1. No Proverty',
+      //     color: 'green',
+      //     bgtext: 'SDG1',
+      //     src: require('./assets/SDG-1.png'),
+      //     products: '2h ago',
+      //     // product: 'heavy rain!'
+      //     // person who posted pic:
+    
+      //   },
+      //    {
+      //     title: '2. Zero Hunger',
+      //     color: 'blue',
+      //     bgtext: 'SDG2',
+      //     src: require('./assets/SDG-2.png'),
+      //     products: '3h ago',
+      //     // product: 'heavy rain!'
+
+      //   }, 
+      //   {
+      //     title: '3. Good Health and Well-being',
+      //     color: 'red',
+      //     bgtext: 'SDG3',
+      //     src: require('./assets/SDG-3.png'),
+      //     products: '4h ago',
+      //     // product: 'heavy rain!'
+
+      //   },
+      //   ],
+
+        cards: [
         {
-          title: '1. No Proverty',
-          color: 'green',
+          title: 'Heavy Rain!',
+          color: 'blue',
           bgtext: 'SDG1',
-          src: require('./assets/SDG-1.png'),
+          src: require('./assets/hawk-1.jpeg'),
           products: '2h ago',
+          liked: 'yes'
           // product: 'heavy rain!'
           // person who posted pic:
     
         },
          {
-          title: '2. Zero Hunger',
-          color: 'blue',
+          title: 'Garden Lightning',
+          color: 'green',
           bgtext: 'SDG2',
-          src: require('./assets/SDG-2.png'),
+          src: require('./assets/hawk-2.jpeg'),
           products: '3h ago',
+          liked: 'yes'
           // product: 'heavy rain!'
 
         }, 
         {
-          title: '3. Good Health and Well-being',
+          title: 'Cleaning your pool',
           color: 'red',
           bgtext: 'SDG3',
-          src: require('./assets/SDG-3.png'),
+          src: require('./assets/hawk-3.jpeg'),
           products: '4h ago',
+          liked: 'yes'
           // product: 'heavy rain!'
 
         },
@@ -59,10 +103,6 @@ export default {
     },
 };
 
-
-
-
-
 </script>
 
 <style>
@@ -70,16 +110,15 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
 
-*{
+/* *{
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  text-align: left;
   }
 
   body{
@@ -102,5 +141,5 @@ export default {
        max-width: 1280px;
        padding: 25px;
        margin: 0 auto;
-       }
+       } */
 </style>
