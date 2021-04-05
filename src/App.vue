f7b7711d743ea7067e76aa16df97891889a17b22
Samet Yalcin <syalcin_@hotmail.com>
2021-04-05 13:40:15 +0200

    Introduction component config

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Introduction />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Introduction from "./components/Introduction.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Introduction,
   },
 };
 </script>
--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br />
+      FTestor a guide and recipes on how to configure / customize this project,<br />
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
         >vue-cli documentation</a
@@ -96,7 +95,7 @@
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: "Introduction",
 };
 </script>
 
