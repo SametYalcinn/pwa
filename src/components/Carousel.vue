ac3b2bd6b0854d405a27d094b170a66be48ec419
joasihe <joasihe90@gmail.com>
2021-04-12 13:32:54 +0200

    end

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,60 +1,17 @@
 <template>
-  <div class="container">
-    <Carousel>
-      <Slide v-for="slide in 10" :key="slide">
-        <div class="carousel__item">{{ slide }}</div>
-      </Slide>
-
-      <template #addons>
-        <!-- <Navigation /> -->
-        <Pagination />
-      </template>
-    </Carousel>
-  </div>
+    <div>
+        <slide :data="slide" />
+    </div>
 </template>
 
 <script>
-import { defineComponent } from "vue";
-import { Carousel, Pagination, Slide } from "vue3-carousel";
-
-import "vue3-carousel/dist/carousel.css";
-
-export default defineComponent({
-  name: "Basic",
-  components: {
-    Carousel,
-    Slide,
-    Pagination,
-  },
-});
+import slide from '@wyhaya/vue-slide'
+export default {
+    data() {
+        return {
+            slide: ['../assets/Naamloos-1.png', '../assets/Naamloos-2.png']
+        }
+    },
+    components: { slide }
+}
 </script>
-
-<style>
-.container {
-  left: 0;
-  right: 0;
-  margin: auto;
-  width: 340px;
-}
-.carousel__item {
-  min-height: 200px;
-  width: 100%;
-  background-color: var(--carousel-color-primary);
-  color: var(--carousel-color-white);
-  font-size: 20px;
-  border-radius: 8px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.carousel__slide {
-  padding: 10px;
-}
-
-.carousel__prev,
-.carousel__next {
-  box-sizing: content-box;
-  border: 5px solid white;
-}
-</style>
