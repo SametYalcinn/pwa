ed7c700327755c872e1b77e06fff07f65d8ee6f8
joasihe <joasihe90@gmail.com>
2021-04-13 10:35:46 +0200

    Imported profile info

---
+++ src/views/Profile.vue
@@ -0,0 +1,25 @@
+<template>
+<h1>Profile</h1>
+ <Info firstName='Samet' lastName='Sanduuuhrrr' picture='../assets/pf/1.jpeg' followers="2" following="30" />
+ <Options />
+ <div class="whitespace"></div>
+</template>
+
+<script>
+import Options from '../components/profile/Options';
+import Info from '../components/profile/Info';
+
+export default {
+  name: "Profile",
+  components: {
+    Options,
+    Info,
+  },
+};
+</script>
+
+<style scoped>
+.whitespace {
+    height: 100px;
+}
+</style>
